1a8395cd9e3af442d13dfc0764003915795416a7
William RAD Funk <contact@WILLIAMRADFunk.com>
2019-12-18 18:48:59 -0800

    Settles on same outline image for pieces regardless of color.

--- front-end/src/app/captured-pieces-panel/captured-pieces-panel.component.ts
+++ front-end/src/app/captured-pieces-panel/captured-pieces-panel.component.ts
@@ -24,50 +24,52 @@ import {
 export class CapturedPiecesPanelComponent implements OnInit {
     @Input() player1Panel: { [key: string]: number[] };
     @Input() player2Panel: { [key: string]: number[] };
+    @Input() playerNumber: number;
     @Input() style: number;
 
   constructor() { }
 
   ngOnInit() { }
 
-    public getClassStyling(value: number, playerNumber: number, index: number): { [key: string]: boolean } {
+    public getClassStyling(value: number, top: boolean, index: number): { [key: string]: boolean } {
+        const panel = top && this.playerNumber === 1 ? this.player2Panel : this.player1Panel;
         const classStyle = {
-            'pawn-1': value === 1 && this.style === 0 && index === this.player1Panel.pawns.length - 1,
-            'pawn-1-outline': value === 1 && this.style === 0 && index !== this.player1Panel.pawns.length - 1,
-            'pawn-2': value === 1 && this.style === 1 && index === this.player1Panel.pawns.length - 1,
-            'pawn-2-outline': value === 1 && this.style === 1 && index !== this.player1Panel.pawns.length - 1,
-            'pawn-3': value === 1 && this.style === 2 && index === this.player1Panel.pawns.length - 1,
-            'pawn-3-outline': value === 1 && this.style === 2 && index !== this.player1Panel.pawns.length - 1,
-            'rook-1': value === 2 && this.style === 0 && index === this.player1Panel.rooks.length - 1,
-            'rook-1-outline': value === 2 && this.style === 0 && index !== this.player1Panel.rooks.length - 1,
-            'rook-2': value === 2 && this.style === 1 && index === this.player1Panel.rooks.length - 1,
-            'rook-2-outline': value === 2 && this.style === 1 && index !== this.player1Panel.rooks.length - 1,
-            'rook-3': value === 2 && this.style === 2 && index === this.player1Panel.rooks.length - 1,
-            'rook-3-outline': value === 2 && this.style === 2 && index !== this.player1Panel.rooks.length - 1,
-            'knight-1': value === 3 && this.style === 0 && index === this.player1Panel.knights.length - 1,
-            'knight-1-outline': value === 3 && this.style === 0 && index !== this.player1Panel.knights.length - 1,
-            'knight-2': value === 3 && this.style === 1 && index === this.player1Panel.knights.length - 1,
-            'knight-2-outline': value === 3 && this.style === 1 && index !== this.player1Panel.knights.length - 1,
-            'knight-3': value === 3 && this.style === 2 && index === this.player1Panel.knights.length - 1,
-            'knight-3-outline': value === 3 && this.style === 2 && index !== this.player1Panel.knights.length - 1,
-            'bishop-1': value === 4 && this.style === 0 && index === this.player1Panel.bishops.length - 1,
-            'bishop-1-outline': value === 4 && this.style === 0 && index !== this.player1Panel.bishops.length - 1,
-            'bishop-2': value === 4 && this.style === 1 && index === this.player1Panel.bishops.length - 1,
-            'bishop-2-outline': value === 4 && this.style === 1 && index !== this.player1Panel.bishops.length - 1,
-            'bishop-3': value === 4 && this.style === 2 && index === this.player1Panel.bishops.length - 1,
-            'bishop-3-outline': value === 4 && this.style === 2 && index !== this.player1Panel.bishops.length - 1,
-            'queen-1': value === 5 && this.style === 0 && index === this.player1Panel.queens.length - 1,
-            'queen-1-outline': value === 5 && this.style === 0 && index !== this.player1Panel.queens.length - 1,
-            'queen-2': value === 5 && this.style === 1 && index === this.player1Panel.queens.length - 1,
-            'queen-2-outline': value === 5 && this.style === 1 && index !== this.player1Panel.queens.length - 1,
-            'queen-3': value === 5 && this.style === 2 && index === this.player1Panel.queens.length - 1,
-            'queen-3-outline': value === 5 && this.style === 2 && index !== this.player1Panel.queens.length - 1
+            'pawn-1': value === 1 && this.style === 0 && index === panel.pawns.length - 1,
+            'pawn-1-outline': value === 1 && this.style === 0 && index !== panel.pawns.length - 1,
+            'pawn-2': value === 1 && this.style === 1 && index === panel.pawns.length - 1,
+            'pawn-2-outline': value === 1 && this.style === 1 && index !== panel.pawns.length - 1,
+            'pawn-3': value === 1 && this.style === 2 && index === panel.pawns.length - 1,
+            'pawn-3-outline': value === 1 && this.style === 2 && index !== panel.pawns.length - 1,
+            'rook-1': value === 2 && this.style === 0 && index === panel.rooks.length - 1,
+            'rook-1-outline': value === 2 && this.style === 0 && index !== panel.rooks.length - 1,
+            'rook-2': value === 2 && this.style === 1 && index === panel.rooks.length - 1,
+            'rook-2-outline': value === 2 && this.style === 1 && index !== panel.rooks.length - 1,
+            'rook-3': value === 2 && this.style === 2 && index === panel.rooks.length - 1,
+            'rook-3-outline': value === 2 && this.style === 2 && index !== panel.rooks.length - 1,
+            'knight-1': value === 3 && this.style === 0 && index === panel.knights.length - 1,
+            'knight-1-outline': value === 3 && this.style === 0 && index !== panel.knights.length - 1,
+            'knight-2': value === 3 && this.style === 1 && index === panel.knights.length - 1,
+            'knight-2-outline': value === 3 && this.style === 1 && index !== panel.knights.length - 1,
+            'knight-3': value === 3 && this.style === 2 && index === panel.knights.length - 1,
+            'knight-3-outline': value === 3 && this.style === 2 && index !== panel.knights.length - 1,
+            'bishop-1': value === 4 && this.style === 0 && index === panel.bishops.length - 1,
+            'bishop-1-outline': value === 4 && this.style === 0 && index !== panel.bishops.length - 1,
+            'bishop-2': value === 4 && this.style === 1 && index === panel.bishops.length - 1,
+            'bishop-2-outline': value === 4 && this.style === 1 && index !== panel.bishops.length - 1,
+            'bishop-3': value === 4 && this.style === 2 && index === panel.bishops.length - 1,
+            'bishop-3-outline': value === 4 && this.style === 2 && index !== panel.bishops.length - 1,
+            'queen-1': value === 5 && this.style === 0 && index === panel.queens.length - 1,
+            'queen-1-outline': value === 5 && this.style === 0 && index !== panel.queens.length - 1,
+            'queen-2': value === 5 && this.style === 1 && index === panel.queens.length - 1,
+            'queen-2-outline': value === 5 && this.style === 1 && index !== panel.queens.length - 1,
+            'queen-3': value === 5 && this.style === 2 && index === panel.queens.length - 1,
+            'queen-3-outline': value === 5 && this.style === 2 && index !== panel.queens.length - 1
         };
-        if (playerNumber === 1) {
+        if (!top) {
             classStyle['black-team-color'] = false;
             classStyle['white-team-color'] = true;
             return classStyle;
-        } else if (playerNumber === 2) {
+        } else if (top) {
             classStyle['black-team-color'] = true;
             classStyle['white-team-color'] = false;
             return classStyle;
